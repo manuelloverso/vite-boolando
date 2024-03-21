ad0eb2f4b75a8af2503769cf796be15de082081e
manuelloverso <manuelloverso@gmail.com>
2024-03-21 16:20:10 +0100

    add header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,31 @@
 <script>
 export default {
   name: "AppHeader",
+
+  data() {
+    return {
+      categories: ["Uomo", "Donna", "Bambino"],
+    };
+  },
 };
 </script>
 
-<template>Il mio Header</template>
+<template>
+  <header>
+    <div class="container">
+      <div class="categories">
+        <a v-for="category in categories" href="#">{{ category }}</a>
+      </div>
+      <div class="logo">
+        <img width="130" src="/images/boolean-logo.png" alt="" />
+      </div>
+      <div class="icons">
+        <i class="fa-regular fa-user icon"></i>
+        <i class="fa-regular fa-heart icon"></i>
+        <i class="fa-solid fa-bag-shopping icon"></i>
+      </div>
+    </div>
+  </header>
+</template>
 
 <style lang="scss"></style>
