5da11539408373b89d0c6b1ecb0b3a3a4073378a
manuelloverso <manuelloverso@gmail.com>
2024-03-21 17:28:32 +0100

    add cards dynamically with object structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,14 +12,14 @@ export default {
 
 <template>
   <header>
-    <div class="container">
+    <div class="my-container">
       <div class="categories">
         <a v-for="category in categories" href="#">{{ category }}</a>
       </div>
       <div class="logo">
         <img width="130" src="/images/boolean-logo.png" alt="" />
       </div>
-      <div class="icons">
+      <div class="icons justify-content-end">
         <i class="fa-regular fa-user icon"></i>
         <i class="fa-regular fa-heart icon"></i>
         <i class="fa-solid fa-bag-shopping icon"></i>
--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,9 +1,91 @@
 <script>
 export default {
   name: "MainCards",
+
+  data() {
+    return {
+      cards: [
+        {
+          img: "/images/1.webp",
+          brand: "Levi's",
+          article: "Relaxed fit tee unisex",
+          price: 29.99,
+          discount: 50,
+        },
+
+        {
+          img: "/images/1.webp",
+          brand: "Levi's",
+          article: "Relaxed fit tee unisex",
+          price: 29.99,
+          discount: 50,
+        },
+
+        {
+          img: "/images/1.webp",
+          brand: "Levi's",
+          article: "Relaxed fit tee unisex",
+          price: 29.99,
+          discount: 50,
+        },
+
+        {
+          img: "/images/1.webp",
+          brand: "Levi's",
+          article: "Relaxed fit tee unisex",
+          price: 29.99,
+          discount: 50,
+        },
+
+        {
+          img: "/images/1.webp",
+          brand: "Levi's",
+          article: "Relaxed fit tee unisex",
+          price: 29.99,
+          discount: 50,
+        },
+
+        {
+          img: "/images/1.webp",
+          brand: "Levi's",
+          article: "Relaxed fit tee unisex",
+          price: 28.99,
+          discount: 50,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    calculatePrice(index) {
+      const price = this.cards[index].price;
+      const discount = this.cards[index].discount;
+      let discounted = 0;
+      if (discount > 0) {
+        discounted = price - (discount * price) / 100;
+      }
+      return discounted.toFixed(2);
+    },
+  },
 };
 </script>
 
-<template>La mia card</template>
+<template>
+  <main>
+    <div class="my-container row row-gap-3 my-3">
+      <div v-for="(card, index) in cards" class="col-4">
+        <div class="card">
+          <img :src="card.img" alt="" />
+          <div class="card-body">
+            <p class="mb-1">{{ card.brand }}</p>
+            <h3>{{ card.article.toUpperCase() }}</h3>
+            <span class="discounted me-3">{{ calculatePrice(index) }}€</span>
+            <span class="full-price">{{ card.price }}€</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
 
 <style lang="scss"></style>
