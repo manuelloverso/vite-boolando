4f1875e2702aafabc24c2f8c9e08c6868a81c379
manuelloverso <manuelloverso@gmail.com>
2024-03-21 17:41:07 +0100

    add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,6 +4,24 @@ export default {
 };
 </script>
 
-<template>Il mio Footer</template>
+<template>
+  <footer>
+    <div class="my-container py-5 d-flex justify-content-between">
+      <div class="legal">
+        <h3>Boolando s.r.l</h3>
+        <span>Informazioni legali</span>
+        <span>Informtiva sulla privacy</span>
+        <span>Diritto di recesso</span>
+      </div>
+      <div class="socials">
+        <h5>Trovaci anche su</h5>
+        <i class="fa-brands fa-square-twitter social-icon"></i>
+        <i class="fa-brands fa-square-facebook social-icon"></i>
+        <i class="fa-brands fa-square-instagram social-icon"></i>
+        <i class="fa-brands fa-square-youtube social-icon"></i>
+      </div>
+    </div>
+  </footer>
+</template>
 
 <style lang="scss"></style>
